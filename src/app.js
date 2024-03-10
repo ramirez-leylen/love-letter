4248073f5b2f69ece0f38cfdd08995b0a88e3041
ramirez-leylen <ramirezleylen@gmail.com>
2024-03-10 00:51:13 -0300

    arreglos render

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.set("views", __dirname+"/views");
 
-const port = 3030;
+const port = process.env.port || 3030;
 
 app.get('/', (req,res) => res.render("index.ejs"));
 
